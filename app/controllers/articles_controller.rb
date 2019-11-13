363457a0a30c749069fc61098fdbd149ae03cb1b
Magomed Saipudinov <saipudinov.magomed@gmail.com>
2019-11-13 15:50:05 +0300

    edit article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,6 +19,19 @@ class ArticlesController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+   @article = Article.find(params[:id])
+    if @article.update_attributes(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
 end
 
 private
