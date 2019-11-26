e99a745e03aaa0b1c4d43ceba981f4bdc60d7f46
Khangerey Saypudinov <saypudinov05@gmail.com>
2019-11-26 15:37:55 +0300

    created comments, sorted descending

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,15 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params.merge(user_id:current_user.id))
+    redirect_to article_path(@article)
+  end
+
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:name,:body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :articles
+  resources :articles do
+    resources :comments, only: [:create]
+  end
   resources :categories do
     resources :articles, only: [:index, :show], module: :categories
   end
